0d45d292c3c43763fd1874765596f3896d21da50
sarah roodin <sarahrhoops34@gmail.com>
2022-10-31 20:00:14 -0400

    Initial Commit

---
+++ quiz app 2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz app 2022
+//
+//  Created by Sarah Roodin on 10/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ quiz app 2022UITests/quiz_app_2022UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  quiz_app_2022UITestsLaunchTests.swift
+//  quiz app 2022UITests
+//
+//  Created by Sarah Roodin on 10/31/22.
+//
+
+import XCTest
+
+class quiz_app_2022UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
