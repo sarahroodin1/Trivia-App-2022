4bf629c47dd616374cb9687ee702c686dca0a5e1
sarah roodin <sarahrhoops34@gmail.com>
2022-12-10 11:39:39 -0500

    changes that have errors

--- quiz app 2022/QuestionViewController.swift
+++ quiz app 2022/QuestionViewController.swift
@@ -12,13 +12,59 @@ class QuestionViewController: UIViewController {
     var question:MCQuestion?
     var questionList:MCQuestionList?
     
+    //if buttons are pressed change title to correct or incorrect
+    @IBAction func AButtonAction(_ sender: Any) {
+        if(AButtonOutlet.isPressed){
+            AButtonOutlet.color = UIColor.red
+            AButtonOutlet.setTitle("Incorrect", for: UIControl.State)
+        }
+    }
+    @IBOutlet weak var AButtonOutlet: UIButton!
+    
+    @IBAction func BButtonAction(_ sender: Any) {
+        if(BButtonOutlet.isPressed){
+            BButtonOutlet.color = UIColor.green
+            BButtonOutlet.setTitle("Correct", for: UIControl.State)
+        }
+    }
+    @IBOutlet weak var BButtonOutlet: UIButton!
+    
+    @IBAction func CButtonAction(_ sender: Any) {
+        if(CButtonOutlet.isPressed){
+            CButtonOutlet.color = UIColor.red
+            CButtonOutlet.setTitle("Incorrect", for: UIControl.State)
+        }
+    }
+    @IBOutlet weak var CButtonOutlet: UIButton!
+    
+    @IBAction func DButtonAction(_ sender: Any) {
+        if(DButtonOutlet.isPressed){
+            DButtonOutlet.color = UIColor.red
+            DButtonOutlet.setTitle("Incorrect", for: UIControl.State)
+        }
+    }
+    @IBOutlet weak var DButtonOutlet: UIButton!
+   
     @IBOutlet weak var questionLabel: UILabel?
         
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.questionLabel?.text = self.questionList?.questions[0].question
+        var randomNum =  Int.random(in: 0..<11)
+        self.questionLabel?.text = self.questionList?.questions[randomNum].question
+        
+        //assigning values from API to buttons
+        
+        //A is always incorrect
+        AButtonOutlet.setTitle(questionList?.questions[randomNum].incorrectAnswer[0], for: .normal)
+        //B is always correct
+        BButtonOutlet.setTitle(questionList?.questions[randomNum].correctAnswer, for: .normal)
+        //C is always incorrect
+        CButtonOutlet.setTitle(questionList?.questions[randomNum].incorrectAnswer[1], for: .normal)
+        //D is always incorrect
+        DButtonOutlet.setTitle(questionList?.questions[randomNum].incorrectAnswer[2], for: .normal)
     }
+
     // Do any additional setup after loading the view.
     }
     
